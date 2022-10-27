e1e311b566ee958616468ad0bec3948fe1ae8640
muskan2532150 <rg2532150@gmail.com>
2022-10-27 17:33:13 +0530

    Added string operator

--- demo.rb
+++ demo.rb
@@ -1,4 +1,4 @@
-# For prints
+For prints
 puts 'Hello'
 p 'world'
 print 'Muskan'
@@ -23,4 +23,19 @@ id = gets.chomp.to_i
 puts "My name is #{name} with id #{id}" 
 
 # ----------------------------------
+str1 =" this is a 
+multi
+line
+string"
 
+str2 = %/This is 
+another 
+method/
+
+puts str1,str2,str1.length,str2.size,str1.upcase,str2.downcase,str1.reverse
+puts str1.include?"this"
+puts "Hi".concat("Kitty")
+# comparsion
+puts str1 == str2 
+puts str1.eql?(str2)
+puts str1[0...5],str1[-2]
