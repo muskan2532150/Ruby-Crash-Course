d63cc2f8ef64a8a4da167002ff36a1dedd054c93
muskan2532150 <rg2532150@gmail.com>
2022-10-27 17:19:56 +0530

    Code for output input comments

---
+++ demo.rb
@@ -0,0 +1,26 @@
+# For prints
+puts 'Hello'
+p 'world'
+print 'Muskan'
+
+# -------------------------------------------
+
+# For Multi-line comments use
+=begin
+C
+O
+M   
+=end
+
+#---------------------------------------
+
+# inputs from user
+p 'Enter a char'
+name = gets.chomp
+# For integer
+p 'Enter a id'
+id = gets.chomp.to_i
+puts "My name is #{name} with id #{id}" 
+
+# ----------------------------------
+
